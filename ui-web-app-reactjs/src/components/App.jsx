90b47087948c4f776b166075963a1c877a0145e5
Rinuf <115867709+Rinuf@users.noreply.github.com>
2023-07-05 21:03:13 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://3.110.62.113/'+e.target.name
+    let url = 'http://3.108.228.118/'+e.target.name
 
     displayData.url=url
     let options = {}
